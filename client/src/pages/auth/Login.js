47298653345ddbcd977ff975b275336e5c18e8cf
tiendung8a6 <tiendung8a6@gmail.com>
2023-09-08 14:20:35 +0700

    Firebase setup

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -8,8 +8,8 @@ import { Link } from "react-router-dom";
 import { createOrUpdateUser } from "../../functions/auth";
 
 const Login = ({ history }) => {
-  const [email, setEmail] = useState("gqlreactnode@gmail.com");
-  const [password, setPassword] = useState("gggggg");
+  const [email, setEmail] = useState("tiendung8a6@gmail.com");
+  const [password, setPassword] = useState("123456");
   const [loading, setLoading] = useState(false);
 
   const { user } = useSelector((state) => ({ ...state }));
