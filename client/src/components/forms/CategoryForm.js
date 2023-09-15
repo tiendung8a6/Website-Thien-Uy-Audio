98d6f68d4e5d6d9967afc23bd76a7c7619f1a496
tiendung8a6 <tiendung8a6@gmail.com>
2023-09-15 23:32:11 +0700

    Admin: Category management

--- client/src/components/forms/CategoryForm.js
+++ client/src/components/forms/CategoryForm.js
@@ -1,21 +1,38 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import { ShoppingOutlined } from '@ant-design/icons';
 
 const CategoryForm = ({ handleSubmit, name, setName }) => (
-  <form onSubmit={handleSubmit}>
-    <div className="form-group">
-      <label>Name</label>
-      <input
+  <Form
+    name="basic"
+    labelCol={{ span: 100 }}
+    wrapperCol={{ span: 160 }}
+    style={{ maxWidth: 2000 }}
+    onFinish={handleSubmit}>
+    <Form.Item
+      label="Tên danh mục"
+      name="name"
+      rules={[{
+        required: true,
+        message: 'Vui lòng nhập tên danh mục!'
+      }]}>
+      <Input
         type="text"
-        className="form-control"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
         autoFocus
-        required
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        prefix={<ShoppingOutlined />}
       />
-      <br />
-      <button className="btn btn-outline-primary">Save</button>
-    </div>
-  </form>
+    </Form.Item>
+
+    <Form.Item
+      wrapperCol={{ offset: 2, span: 10 }}
+    >
+      <Button type="primary" htmlType="submit" className="ml-2">
+        Lưu lại
+      </Button>
+    </Form.Item>
+  </Form>
 );
 
 export default CategoryForm;
