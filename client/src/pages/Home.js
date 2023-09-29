3e4ad76f5d72779cf942838f9c7f760976baafda
Kin9989 <HaoQH@int.t4tek.co>
2023-09-29 10:10:27 +0700

    update2

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,56 +11,96 @@ import RegisterNew from '../pages/auth/RegisterNew/RegisterNew'
 import Banner2 from "../components/home/Banner2";
 import Footer from "../components/footer/Footer";
 import SpeedDial from "../components/SpeedDial/SpeedDial";
-import AdProduct from "../components/adProduct/AdProduct";
-
-import ScrollAnimation from 'react-animate-on-scroll';
-import "animate.css/animate.min.css";
-
+import ScrollAnimation from "../components/ScrollAnimatiion/ScrollAnimation";
+import './Home.css'
 const Home = () => {
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", reveal);
   return (
     <>
-      <ScrollAnimation animateIn='wobble'
-        initiallyVisible={true}>
-        <h1>
-          initiallyVisible
-        </h1>
-      </ScrollAnimation>
       <Carousel></Carousel>
-      {/* <div className="jumbotron text-danger h1 font-weight-bold text-center">
-        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
-      </div> */}
-
       <Banner2></Banner2>
-      <h4 className="text-center p-3  mb-5 display-4 jumbotron animate__animated animate__bounce">
-        Sản Phẩm Mới Nhất
-      </h4>
+      <section>
+        <div className="container reveal">
+          <h4 className="text-center p-3  mb-5 display-4 jumbotron">
+            Sản Phẩm Mới Nhất
+          </h4>
+          <div className="text-container">
+            <div className="text-box">
+              <NewArrivals />
+            </div>
 
-      <NewArrivals />
+          </div>
+        </div>
+      </section>
 
-      <ScrollAnimation animateIn='flipInY'
-        animateOut='flipOutY'>
-        <h4 className="text-center p-3 mt-5 mb-5 display-4 ">
-          Sản Phẩm Bán Chạy
-        </h4>
-      </ScrollAnimation>
+      <section>
+        <div className="container reveal">
+          <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+            Sản Phẩm Bán Chạy
+          </h4>
+          <div className="text-container">
+            <div className="text-box">
+              <BestSellers />
+            </div>
 
-      <AdProduct />
-      <BestSellers />
+          </div>
+        </div>
+      </section>
 
-      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
-        Danh Mục Sản Phẩm
-      </h4>
+      <section>
+        <div className="container reveal">
+          <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+            Danh Mục Sản Phẩm
+          </h4>
+          <div className="text-container">
+            <div className="text-box">
+              <CategoryList />
+            </div>
+
+          </div>
+        </div>
+      </section>
       {/* 
       <Watermark content="Ant Design">
         <div style={{ height: 500 }} />
       </Watermark> */}
-      <CategoryList />
+
 
       {/* <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron ">
         Danh Mục Con
       </h4>
       <SubList /> */}
-      <CarouselCardCustomer></CarouselCardCustomer>
+
+      <section>
+        <div className="container reveal">
+          <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+            Phản hồi khách hàng
+          </h4>
+          <div className="text-container">
+            <div className="text-box">
+              <CarouselCardCustomer></CarouselCardCustomer>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
 
       <RegisterNew></RegisterNew>
       <br />
