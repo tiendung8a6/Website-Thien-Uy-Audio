ccf564cba0b8d301d529f64559731f9d4abb56cd
Kin9989 <HaoQH@int.t4tek.co>
2023-09-30 18:24:07 +0700

    update

--- client/src/components/ScrollAnimatiion/ScrollAnimation.js
+++ client/src/components/ScrollAnimatiion/ScrollAnimation.js
@@ -1,57 +1,44 @@
 import React from "react";
 import './scrollanimation.css'
 const ScrollAnimation = () => {
-    function reveal() {
-        var reveals = document.querySelectorAll(".reveal");
-
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
-
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-
-    window.addEventListener("scroll", reveal);
 
     return (
         <div>
-            <section>
-                <div className="container reveal">
-                    <h2>Caption</h2>
-                    <div className="text-container">
-                        <div className="text-box">
-                            <h3>Section Text</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                                eius molestiae perferendis eos provident vitae iste.
-                            </p>
-                        </div>
-                        <div className="text-box">
-                            <h3>Section Text</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                                eius molestiae perferendis eos provident vitae iste.
-                            </p>
-                        </div>
-                        <div className="text-box">
-                            <h3>Section Text</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-                                eius molestiae perferendis eos provident vitae iste.
-                            </p>
-                        </div>
-                    </div>
+            <section className="hero">
+                <div className="hero-inner" id="section-0">
+                    <figure />
+                    <h2 className="hero__title">Explore our world</h2>
+                </div>
+                <div className="hero-inner" id="section-1">
+                    <figure />
+                    <h2 className="hero__title">View all its beauty</h2>
+                </div>
+                <div className="hero-inner" id="section-2">
+                    <figure />
+                    <h2 className="hero__title">Take lots of photos</h2>
+                </div>
+                <div className="hero-inner" id="section-3">
+                    <figure />
+                    <h2 className="hero__title">Each one of them pretty</h2>
                 </div>
             </section>
-            
-            
+            <section className="content">
+                <article className="content__inner">
+                    <h1 className="content__title">Some Happy Little Text</h1>
+                    <h3 className="content__author">By Bob Ross</h3>
+                    <p>A thin paint will stick to a thick paint. The shadows are just like the highlights, but we're going in the opposite direction. When you do it your way you can go anywhere you choose.</p>
+                    <p>You have to make almighty decisions when you're the creator. When you buy that first tube of paint it gives you an artist license. There we go. The little tiny Tim easels will let you down. Let's just drop a little Evergreen right here.</p>
+                    <p>Trees grow in all kinds of ways. They're not all perfectly straight. Not every limb is perfect. I really believe that if you practice enough you could paint the 'Mona Lisa' with a two-inch brush. Just pretend you are a whisper floating across a mountain. This is your world, whatever makes you happy you can put in it. Go crazy.</p>
+                    <p>The first step to doing anything is to believe you can do it. See it finished in your mind before you ever start. We'll play with clouds today. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals. Don't kill all your dark areas - you need them to show the light. We don't want to set these clouds on fire. Every day I learn.</p>
+                    <blockquote cite="Bob Ross">When you do it your way you can go anywhere you choose.</blockquote>
+                    <p>Let's get wild today. Once you learn the technique, ohhh! Turn you loose on the world; you become a tiger. This is probably the greatest thing to happen in my life - to be able to share this with you.</p>
+                    <p>I'm a water fanatic. I love water. This is gonna be a happy little seascape. Put light against light - you have nothing. Put dark against dark - you have nothing. It's the contrast of light and dark that each give the other one meaning. Let's put a touch more of the magic here. I'm going to mix up a little color. We’ll use Van Dyke Brown, Permanent Red, and a little bit of Prussian Blue.</p>
+                    <p>And that's when it becomes fun - you don't have to spend your time thinking about what's happening - you just let it happen. Let all these things just sort of happen. Don't fight it, use what happens. Let's put some happy little bushes on the other side now.</p>
+                    <blockquote cite="Bob Ross">Don't fight it, use what happens.</blockquote>
+                </article>
+            </section>
         </div>
+
 
     );
 }
