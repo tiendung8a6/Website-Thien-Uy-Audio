4c5758739bda87252750e6f940f2a51a710076ab
tiendung8a6 <tiendung8a6@gmail.com>
2023-12-05 10:21:32 +0700

    Fix Bug Firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCbSLqeYcpLbelOPrJVxmPQvAPnKUWy14",
-  authDomain: "ecommerce-bdf98.firebaseapp.com",
-  projectId: "ecommerce-bdf98",
-  storageBucket: "ecommerce-bdf98.appspot.com",
-  messagingSenderId: "171919899530",
-  appId: "1:171919899530:web:ae4b7b58f17b7398e26673",
-  measurementId: "G-4PV55SEFZ8"
+  apiKey: "AIzaSyAOwKG6g1ctOQKLt4ZeX1bC7vkX9GctILk",
+  authDomain: "thienuyaudio-1e259.firebaseapp.com",
+  projectId: "thienuyaudio-1e259",
+  storageBucket: "thienuyaudio-1e259.appspot.com",
+  messagingSenderId: "720757308900",
+  appId: "1:720757308900:web:a6f33b0418139378801bf1",
+  measurementId: "G-ZZCF5BLGE1"
 };
 
 // Initialize Firebase
--- server/firebase/index.js
+++ server/firebase/index.js
@@ -3,10 +3,6 @@ var admin = require("firebase-admin");
 var serviceAccount = require("../config/fbServiceAccountKey.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ecommerce-bdf98-default-rtdb.firebaseio.com"
-  // databaseURL: "https://ecommerce-bdf98.firebaseio.com"
-
+  credential: admin.credential.cert(serviceAccount)
 });
-
 module.exports = admin;
